d5da981408a9f4b4a4200276bfecb53429a43db4
micail <michaelpaczkowski@gmail.com>
2020-06-09 07:15:50 +0100

    Add sorting function to the Table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,20 +3,42 @@ import React, { useState } from 'react';
 import './Table.scss';
 
 const Table = ({ entries }) => {
+  const [sortedBy, setSortedBy] = useState(null);
+
+  let sorted = [...entries];
+  
+  if(sortedBy !== null) {
+    sorted.sort((a, b) => {
+      if (a[sortedBy] < b[sortedBy]) {
+        return -1;
+      }
+      if (a[sortedBy] > b[sortedBy]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
+          <th>Name
+          <button type="button" onClick={() => setSortedBy('name')}>
+            </button>
+          </th>
           <th>Surname</th>
           <th>email</th>
           <th>Phone</th>
           <th>Age</th>
-          <th>City</th>
+          <th>City
+          <button type="button" onClick={() => setSortedBy('city')}>
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {entries.map(entry => (
+        {sorted.map(entry => (
           <tr key={entry.id}>
             <td>{entry.name}</td>
             <td>{entry.surname}</td>
